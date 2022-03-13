bed603acff845a80084307a39f68b62f8463cd93
andywokzyrg <andywokzyrg>
2022-03-13 21:44:35 +0300

    fonts added, user-item.component added, select.component addded

--- src/app/shared/components/user-item/user-item.component.ts
+++ src/app/shared/components/user-item/user-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-user-item',
-  templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.scss']
+    selector: 'app-user-item',
+    templateUrl: './user-item.component.html',
+    styleUrls: ['./user-item.component.scss']
 })
 export class UserItemComponent implements OnInit {
+    @Input() avatar: string | undefined
+    @Input() username: string | undefined
+    @Input() balance: string | undefined
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
--- src/app/shared/components/user-item/user-item.module.ts
+++ src/app/shared/components/user-item/user-item.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core";
 import {UserItemComponent} from "./user-item.component";
 import {CommonModule} from "@angular/common";
+import {SelectModule} from "../../ui/select/select.module";
 
 @NgModule({
     declarations: [UserItemComponent],
     exports: [UserItemComponent],
-    imports: [CommonModule]
+    imports: [CommonModule, SelectModule]
 })
 export class UserItemModule {
 }
